539617bc63e4d330ac8de3d49ca33d533c806ce7
DESKTOP-F47U60N\gabri <gabrielahercules94@gmail.com>
2025-06-21 14:11:24 -0600

    Avance final

--- controllers/recipesController.js
+++ controllers/recipesController.js
@@ -3,7 +3,7 @@ const { createClient } = require("@supabase/supabase-js");
 
 const supabaseAnonClient=createClient(
     process.env.SUPABASE_URL,
-    process.env.SUPABASE_ANON_KEY
+    process.env.SUPABASE_SERVICE_ROLE
 
 );
 
@@ -13,38 +13,45 @@ exports.getALLRecipes= async(req,res)=>{
     .from("recipes")
     .select("*");
     if (error) throw error;
-    res.status(200).json({data});
+    return res.status(200).json({data});
     }catch(err){
-        res.status(err.status|| 500).json({error: err.message});
+       return res.status(err.status|| 500).json({error: err.message});
     }
-    return res;
 };
 exports.createRecipes= async(req, res)=>{
     try{
-    const{title,ingredients,preparation,image}=req.body;
+    const{image,title,ingredients,preparation}=req.body;
+ 
     const{data, error}= await supabaseAnonClient
     .from('recipes')
-    .insert({title,ingredients,preparation,image});
+    .insert({image, title, preparation,ingredients})
+    .select();
+    if (error) throw error;
+        return res.status(201).json({ data });
     }catch(err){
        return res.status(err.status||500).json({error: err.message});
     }
-    return res;
 };
 exports.updateRecipes= async(req, res)=>{
     try {
     const { id } = req.params;
-    const { title, ingredients, preparation,image } = req.body;
+    const { image,title, ingredients, preparation } = req.body;
 
     const { data, error } = await supabaseAnonClient
       .from("recipes")
-      .update({ title,ingredients,preparation,image  })
-      .eq("id", id);
+      .update({ image,title,ingredients,preparation  })
+      .eq("id", id)
+      .select();
 
     if (error) throw error;
-    res.status(200).json({ data: data[0] });
+    if (!data || data.length === 0) {
+      return res.status(404).json({ error: "Receta no encontrada" });
+    }
+    return res.status(200).json({ data: data[0] });
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
+
 };
 exports.deleteRecipes= async(req, res)=>{
     try {
@@ -55,9 +62,9 @@ exports.deleteRecipes= async(req, res)=>{
       .eq("id", id);
 
     if (error) throw error;
-    res.status(204).send(); // No content en response
+   return res.status(204).send(); // No content en response
   } catch (err) {
-    res.status(400).json({ error: err.message });
+    return res.status(400).json({ error: err.message });
   }
 };
 
--- routes/recipesRoutes.js
+++ routes/recipesRoutes.js
@@ -1,12 +1,11 @@
 const express = require('express');
-// const { registerUser, loginUser } = require('../controllers/authController');
 const{getALLRecipes, createRecipes, updateRecipes, deleteRecipes}=require('../controllers/recipesController');
 const router = express.Router();
 
 router.get("/allRecipes",getALLRecipes);
 router.post("/newRecipes",createRecipes);
-router.put("/update/:id", updateRecipes);
-router.delete("/delete/:id", deleteRecipes);
+router.put("/updateRecipes/:id", updateRecipes);
+router.delete("/deleteRecipes/:id", deleteRecipes);
 
 
 module.exports = router;
